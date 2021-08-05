219a5b8020f2c9cf9733e9ee453d735e07953a99
Ondřej Sláma <ondrej.slama@rieter.com>
2021-08-05 15:43:32 +0200

    Added basic test file

---
+++ test.py
@@ -0,0 +1,15 @@
+""" File for basic testing of flask API """
+import requests
+
+flask_endpoint = 'http://127.0.0.1:5000/secret'
+
+# Put it here just for tests
+test_user = 'myUser123'
+test_password = 'secretSecret'
+
+def main():
+    response = requests.get(flask_endpoint, auth=(test_user, test_password))
+    print(response)
+
+if __name__ == '__main__':
+    main()
