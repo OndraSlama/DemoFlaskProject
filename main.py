38ff43b8be04434ffd146f2e3e5cc7b0e5206af7
Ondřej Sláma <ondrej.slama@rieter.com>
2021-08-05 15:10:59 +0200

    - Added basic authentication check (test) - added new dependency to requirements.txt

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import flask
 import os
+from flask_basicauth import BasicAuth
 
 app = flask.Flask(__name__)
 
+app.config['BASIC_AUTH_USERNAME'] = os.environ.get("USER_NAME")
+app.config['BASIC_AUTH_PASSWORD'] = os.environ.get("USER_PASS")
+basic_auth = BasicAuth(app)
+
 @app.route('/')
 def index():
-    return f'Hello, World!'
+    return f'Hello, World! User {os.environ.get("USER_NAME")}'
 
+@app.route('/secret')
+@basic_auth.required
+def secret_index():
+    return f'Secret stuff! User {os.environ.get("USER_NAME")}'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
