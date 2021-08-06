bf91c40f4004d8dbb0af497ce38200d608a2baf6
Ondřej Sláma <ondrej.slama@rieter.com>
2021-08-06 21:43:13 +0200

    Updated simple test

--- tests/test.py
+++ tests/test.py
@@ -1,15 +1,16 @@
 """ File for basic testing of flask API """
 import requests
 
-flask_endpoint = 'http://127.0.0.1:5000/secret'
+flask_endpoint = 'http://127.0.0.1:5000/export/123456'
 
-# Put it here just for tests
+# Put it here just for test sake
 test_user = 'myUser123'
-test_password = 'secretSecret__'
+test_password = 'secretSecret'
 
 def main():
     response = requests.get(flask_endpoint, auth=(test_user, test_password))
-    print(response.text)
+    assert response.status_code == 200, 'Request did not finished sucessfully'
+
 
 if __name__ == '__main__':
     main()
